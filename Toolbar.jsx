83cacb2ff206758ff28d9ea054018c8c54336b8f
VimarshKhandkar0212 <vimarshkhandkar2@gmail.com>
2026-02-02 22:40:17 +0530

    Update Toolbar.jsx

--- Toolbar.jsx
+++ Toolbar.jsx
@@ -7,7 +7,17 @@ import {
 } from "react-icons/fa";
 import "../Whiteboard.css";
 
-const Toolbar = ({ tool, setTool, undo, redo, clearCanvas }) => {
+const Toolbar = ({
+  tool,
+  setTool,
+  undo,
+  redo,
+  clearCanvas,
+  pencilColor,
+  setPencilColor,
+  pencilWidth,
+  setPencilWidth
+}) => {
   return (
     <div className="toolbar">
       <button
@@ -23,6 +33,25 @@ const Toolbar = ({ tool, setTool, undo, redo, clearCanvas }) => {
       >
         <FaEraser />
       </button>
+
+      {/* 🎨 Pencil options */}
+      {tool === "pencil" && (
+        <div className="pencil-options">
+          <input
+            type="color"
+            value={pencilColor}
+            onChange={(e) => setPencilColor(e.target.value)}
+          />
+
+          <input
+            type="range"
+            min="1"
+            max="15"
+            value={pencilWidth}
+            onChange={(e) => setPencilWidth(+e.target.value)}
+          />
+        </div>
+      )}
 
       <div className="tool-separator" />
 
